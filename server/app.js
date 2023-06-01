abddbe7ad22a7daf9805432c37be6974ec2c8b6c
jonortega <jon.ortega.g@gmail.com>
2023-06-01 14:06:13 +0200

    Carga automatica de preguntasNiveles

--- server/app.js
+++ server/app.js
@@ -15,12 +15,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static("uploads"))
 
 
-/*===============INICIALIZAR MONGODB====================
+/*===============INICIALIZAR MONGODB====================*/
 db.createCollection('users', (err, collection) => {
     if (err) {
         console.error(err);
     } else {
-        console.log('Colección creada:', collection);
+        console.log('Colección users creada:', collection);
     }
 });
 
@@ -28,13 +28,28 @@ db.createCollection('screening', (err, collection) => {
     if (err) {
         console.error(err);
     } else {
-        console.log('Colección creada:', collection);
+        console.log('Colección screening creada:', collection);
     }
 });
 
 setTimeout(function () {
     const screeningData = require('/docker-entrypoint-initdb.d/screening.json');
     db.screening.insertMany(screeningData);
+    console.log("Datos cargados a screening");
+}, 5000); // 5000 ms = 5 s
+
+db.createCollection('preguntasNiveles', (err, collection) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Colección preguntasNiveles creada:', collection);
+    }
+});
+
+setTimeout(function () {
+    const preguntasNivelesData = require('/docker-entrypoint-initdb.d/preguntasNiveles.json');
+    db.preguntasNiveles.insertMany(preguntasNivelesData);
+    console.log("Datos cargados a preguntasNiveles");
 }, 5000); // 5000 ms = 5 s
 /*===============INICIALIZAR MONGODB====================*/
 
